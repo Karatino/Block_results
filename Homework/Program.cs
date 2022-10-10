53bb8ae91205254e8f2ec41081c4fe8bd95c77e2
Karatino <viewkaratsonic@mail.ru>
2022-10-10 15:52:42 +0500

    Added a method for filling in the second array according to the task

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -3,5 +3,25 @@
 либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами*/
 
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
